ff925b0453f2f23efa21766c03ad1cf874676150
Dustin Diaz <polvero@gmail.com>
2014-02-20 12:48:54 -0800

    moves `client` into scaffold

--- client/controllers/application-controller.js
+++
@@ -1,9 +0,0 @@
-provide('client/controllers/application-controller', function () {
-  var klass = require('klass')
-  return klass()
-  .methods({
-    render: function (view, options) {
-      $.id('doc').html(view(options || {}))
-    }
-  })
-}())
--- client/controllers/fixture-controller.js
+++
@@ -1,7 +0,0 @@
-provide('client/controllers/fixture-controller', function () {
-  var ApplicationController = require('client/controllers/application-controller')
-  return ApplicationController.extend()
-    .methods({
-      home: function (context) {}
-    })
-}())
