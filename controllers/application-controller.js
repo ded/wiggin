1fc3edbed56992616c636490858390b2fa77daab
Dustin Diaz <polvero@gmail.com>
2014-02-26 15:21:00 -0800

    new `this.req` and `this.res` on controllers

--- controllers/application-controller.js
+++ controllers/application-controller.js
@@ -1,35 +1,12 @@
-var fs = require('fs')
-  , p = require('path')
-  , klass = require('klass')
-  , jade = require('jade')
-  , qs = require('qs')
-  , v = require('valentine')
-  , when = require('when')
+var klass = require('klass')
 
 module.exports = klass(function (app) {
   this.app = app
   this.beforeFilters = []
 })
 .methods({
-  render: function (req, res, next) {
-    var modelAction = this.app.locals.config.routes[req.route.path][req.method.toLowerCase()]
-    var modelParts = modelAction.split('.')
-    var model = modelParts[0]
-    var action = modelParts[1]
-    var Model = require(this.app.locals.config.models + '/' + model + '-model')
-
-    function success(locals) {
-      res.render(this.app.locals.config.routes[req.route.path].template, locals)
-    }
-
-    function reject(reason) {
-      reason.status = 500
-      next(reason)
-    }
-
-    when(Model[action](req))
-      .then(success.bind(this))
-      .otherwise(reject)
+  render: function (template, locals) {
+    this.res.render(template, locals)
   }
 , json: function (req, res, data) {
     data = '])}while(1);</x>' + JSON.stringify(data)
@@ -40,47 +17,6 @@ module.exports = klass(function (app) {
     this.beforeFilters.push({
       action: action,
       handler: handler
-    })
-  }
-, partial: function (req, res) {
-    req.query.params = qs.parse(req.query.params)
-    var self = this
-    var path = '/' + req.params.path
-    var paramPath = path
-    var canonicalPath = path
-    v(req.query.params).each(function (key, val) {
-      paramPath += '/:' + key
-      canonicalPath += '/' + val
-    })
-
-    var routeParamsPath = this.app.locals.config.routes[paramPath]
-    var template = p.normalize(this.app.locals.config.views + '/' + routeParamsPath.template + '.jade')
-    var modelAction = routeParamsPath[req.method.toLowerCase()]
-    var modelParts = modelAction.split('.')
-    var Model = require(this.app.locals.config.models + '/' + modelParts[0] + '-model')
-    var source = fs.readFileSync(template, 'utf8')
-    var sequence = false
-    var block = source.split('\n').filter(function (line) {
-      if (line.match(/^block content/)) sequence = true
-      if (sequence && (line.match(/^\s/) || line.match(/^block content/))) {
-        return line.match(/^\s/)
-      } else {
-        return sequence = false
-      }
-    })
-    .map(function (line) {
-      return line.replace(/^  /, '')
-    })
-    .join('\n')
-    req.params = req.query.params
-    Model[modelAction](req).then(function (locals) {
-      var options = v.extend({ filename: template }, locals, self.app.locals, res.locals)
-      var html = jade.render(block, options)
-      res.json({
-        html: html,
-        pagetitle: options.pagetitle,
-        route: canonicalPath
-      })
     })
   }
 })
