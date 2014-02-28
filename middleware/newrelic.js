b38cad4550714d262bfeb4bfa665cfa7826b4fd5
Dustin Diaz <polvero@gmail.com>
2014-02-28 15:05:11 -0800

    updates mounter to make use of `*` filter remove newrelic middleware

--- middleware/newrelic.js
+++
@@ -1,8 +0,0 @@
-module.exports = function (client) {
-  return function (req, res, next) {
-    var r = req.originalRoute
-    client.setTransactionName(r.name)
-    client.setControllerName(r.controller, r.action)
-    next()
-  }
-}
