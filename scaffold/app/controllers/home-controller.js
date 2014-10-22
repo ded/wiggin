92613eafee475b3548c25a445124a4636dace3c6
Dustin Diaz <polvero@gmail.com>
2014-10-21 17:34:29 -0700

    indent

--- scaffold/app/controllers/home-controller.js
+++ scaffold/app/controllers/home-controller.js
@@ -1,8 +1,8 @@
-module.exports = require('wiggin/controllers/application-controller').extend()
-  .methods({
-    show: function (next) {
-      this.render('home/show', {
-        title: 'Welcome to the Wiggin Framework'
-      })
-    }
-  })
+module.exports = require('app/controllers/application-controller').extend()
+.methods({
+  show: function (next) {
+    this.render('home/show', {
+      title: 'Welcome to the Wiggin Framework'
+    })
+  }
+})
